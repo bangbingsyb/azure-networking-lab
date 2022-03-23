6664f809f660d0a7412e5f3398a4286601fcdbb1
Yanbing Shi <bangbing@gmail.com>
2022-03-23 16:54:36 -0700

    Add nontransitive lagb

--- vnet-peering/lab1-cross-subscription-peering/step2-deploy-peering.sh
+++ vnet-peering/lab1-cross-subscription-peering/step2-deploy-peering.sh
@@ -2,11 +2,11 @@ az login
 
 export SUB1="bc3b91ed-1253-42b5-826d-de20de48b2d9"
 export REGION1="westus2"
-export RG1="vnet1-$REGION1-2"
+export RG1="vnet1-$REGION1"
 
 export SUB2="e3853e83-0d02-4fb3-b88f-05b5fd21aee2"
-export REGION2="eastus2"
-export RG2="vnet2-$REGION2-2"
+export REGION2="southcentralus"
+export RG2="vnet2-$REGION2"
 
 az account set --subscription $SUB1
 az deployment group create --resource-group $RG1 --template-file step2-peering.json --parameters step2-peering-vnet1.parameters.json --parameters targetVNetSubscriptionId=$SUB2 targetVNetResourceGroupName=$RG2 --verbose
---
+++ vnet-peering/lab2-non-transitive-connectivity/step1-deploy-vnets.sh
@@ -0,0 +1,19 @@
+az login
+
+ADMINPASSWORD=$1
+if [ -z "${ADMINPASSWORD}" ];
+then
+    echo "VM admin password must be provided."
+    exit 32
+fi
+
+export SUB="bc3b91ed-1253-42b5-826d-de20de48b2d9"
+export REGION="westus2"
+export RG="nontransitive-connectivity"
+
+az account set --subscription $SUB
+az group create --name $RG --location $REGION
+az deployment group create --resource-group $RG --template-file step1-vnet.json --parameters step1-vnet1.parameters.json --parameters adminPassword=$ADMINPASSWORD --verbose
+az deployment group create --resource-group $RG --template-file step1-vnet.json --parameters step1-vnet2.parameters.json --parameters adminPassword=$ADMINPASSWORD --verbose
+az deployment group create --resource-group $RG --template-file step1-vnet.json --parameters step1-vnet3.parameters.json --parameters adminPassword=$ADMINPASSWORD --verbose
+
