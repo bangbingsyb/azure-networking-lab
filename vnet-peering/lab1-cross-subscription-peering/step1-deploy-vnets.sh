c01a988f0aa2334107c2b09018519c6a95e202b9
Yanbing Shi <bangbing@gmail.com>
2022-03-23 16:11:02 -0700

    Add VNet peering lab1

---
+++ vnet-peering/lab1-cross-subscription-peering/step1-deploy-vnets.sh
@@ -0,0 +1,24 @@
+az login
+
+ADMINPASSWORD=$1
+if [ -z "${ADMINPASSWORD}" ];
+then
+    echo "VM admin password must be provided."
+    exit 32
+fi
+
+export SUB1="bc3b91ed-1253-42b5-826d-de20de48b2d9"
+export REGION1="westus2"
+export RG1="vnet1-$REGION1-2"
+
+export SUB2="e3853e83-0d02-4fb3-b88f-05b5fd21aee2"
+export REGION2="eastus2"
+export RG2="vnet2-$REGION2-2"
+
+az account set --subscription $SUB1
+az group create --name $RG1 --location $REGION1
+az deployment group create --resource-group $RG1 --template-file step1-vnet.json --parameters step1-vnet1.parameters.json --parameters adminPassword=$ADMINPASSWORD --verbose
+
+az account set --subscription $SUB2
+az group create --name $RG2 --location $REGION2
+az deployment group create --resource-group $RG2 --template-file step1-vnet.json --parameters step1-vnet2.parameters.json --parameters adminPassword=$ADMINPASSWORD --verbose
---
+++ vnet-peering/lab1-cross-subscription-peering/step2-deploy-peering.sh
@@ -0,0 +1,15 @@
+az login
+
+export SUB1="bc3b91ed-1253-42b5-826d-de20de48b2d9"
+export REGION1="westus2"
+export RG1="vnet1-$REGION1-2"
+
+export SUB2="e3853e83-0d02-4fb3-b88f-05b5fd21aee2"
+export REGION2="eastus2"
+export RG2="vnet2-$REGION2-2"
+
+az account set --subscription $SUB1
+az deployment group create --resource-group $RG1 --template-file step2-peering.json --parameters step2-peering-vnet1.parameters.json --parameters targetVNetSubscriptionId=$SUB2 targetVNetResourceGroupName=$RG2 --verbose
+
+az account set --subscription $SUB2
+az deployment group create --resource-group $RG2 --template-file step2-peering.json --parameters step2-peering-vnet2.parameters.json --parameters targetVNetSubscriptionId=$SUB1 targetVNetResourceGroupName=$RG1 --verbose
