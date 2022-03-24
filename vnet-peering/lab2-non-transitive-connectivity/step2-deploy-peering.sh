b8d79b06f4770d011f0295884d276f52639610b2
Yanbing Shi <bangbing@gmail.com>
2022-03-23 23:46:28 -0700

    Add hub spoke

--- vnet-peering/lab2-non-transitive-connectivity/step2-deploy-peering.sh
+++ vnet-peering/lab2-non-transitive-connectivity/step2-deploy-peering.sh
@@ -2,8 +2,8 @@ az login
 
 export SUB="bc3b91ed-1253-42b5-826d-de20de48b2d9"
 export REGION="westus2"
-export RG="nontransitive-connectivity"
+export RG="demo-non-transitive"
 
 az account set --subscription $SUB
-az deployment group create --resource-group $RG --template-file step2-peering.json --parameters step2-peering-vnet1.parameters.json --parameters targetVNetSubscriptionId=$SUB targetVNetResourceGroupName=$RG --verbose
-az deployment group create --resource-group $RG --template-file step2-peering.json --parameters step2-peering-vnet2.parameters.json --parameters targetVNetSubscriptionId=$SUB targetVNetResourceGroupName=$RG --verbose
+az deployment group create --resource-group $RG --template-file step2-peering.json --parameters step2-peering-vnet1.parameters.json 
+az deployment group create --resource-group $RG --template-file step2-peering.json --parameters step2-peering-vnet2.parameters.json
