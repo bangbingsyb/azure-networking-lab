3567eb55cbc07cf186ddfe79138a4a57e3110263
Yanbing Shi <bangbing@gmail.com>
2022-03-24 02:29:21 -0700

    add vwan lab

--- vnet-peering/lab3-hub-spoke-vnet/step1-deploy-hub.sh
+++ vnet-peering/lab3-hub-spoke-vnet/step1-deploy-hub.sh
@@ -9,7 +9,7 @@ fi
 
 export SUB="bc3b91ed-1253-42b5-826d-de20de48b2d9"
 export REGION="westus2"
-export RG="demo-hub-spoke"
+export RG="demo-hub-spoke-vnet"
 
 az account set --subscription $SUB
 az group create --name $RG --location $REGION
--- vnet-peering/lab3-hub-spoke-vnet/step2-deploy-spokes.sh
+++ vnet-peering/lab3-hub-spoke-vnet/step2-deploy-spokes.sh
@@ -9,7 +9,7 @@ fi
 
 export SUB="bc3b91ed-1253-42b5-826d-de20de48b2d9"
 export REGION="westus2"
-export RG="demo-hub-spoke"
+export RG="demo-hub-spoke-vnet"
 
 az account set --subscription $SUB
 az deployment group create --resource-group $RG --template-file step2-spoke.json --parameters step2-spoke1.parameters.json --parameters adminPassword=$ADMINPASSWORD --verbose
